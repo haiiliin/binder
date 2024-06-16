ff9dc04c398733bf40f43bd8223308c87fc3e93d
Hailin Wang <hailin.wang@connect.polyu.hk>
2024-06-16 18:52:05 +0800

    Specify the python tag and abi tag

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
-from setuptools import setup, Extension
+from setuptools import setup
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
-setup(ext_modules=[Extension(name="binder.fakemodule", sources=[])])
+
+class bdist_wheel(_bdist_wheel):
+    def finalize_options(self):
+        super().finalize_options()
+        self.root_is_pure = False
+        self.python_tag = "py3"
+        self.py_limited_api = "cp38"
+
+
+setup(cmdclass={"bdist_wheel": bdist_wheel})
