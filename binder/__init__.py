ef115a98b3edbe62a8dbe1e8892f88a9cc0a6796
Hailin Wang <hailin.wang@connect.polyu.hk>
2024-06-18 15:36:09 +0800

    [skip ci] Do not use iterable unpacking

--- binder/__init__.py
+++ binder/__init__.py
@@ -15,7 +15,7 @@ BINDER_BIN_DIR = os.path.join(os.path.dirname(__file__), "bin")
 
 
 def binder():
-    raise SystemExit(subprocess.call([os.path.join(BINDER_BIN_DIR, "binder"), *sys.argv[1:]], close_fds=False))
+    raise SystemExit(subprocess.call([os.path.join(BINDER_BIN_DIR, "binder")] + sys.argv[1:], close_fds=False))
 
 
 def _get_version():
